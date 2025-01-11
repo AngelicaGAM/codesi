7d2b00a906858df0ce00963081f89315eebea242
mjayalam <martin.ayala0297@gmail.com>
2025-01-11 12:01:26 -0500

    codesi final

--- src/components/PartnerCompany.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import './PartnerCompany.css';
-const BASE_URL = '/images';
-const PartnerCompany = () => {
-    return (_jsxs("section", { className: 'PartnerCompany', children: [_jsx("p", { className: 'titlesGC title', children: "Empresas de GRUPO CODESI" }), _jsxs("div", { className: 'imagesdiv', children: [_jsx("a", { href: "https://www.instagram.com/tyrionburgers?igsh=emF3ZndvOXV6MnNy", target: "_blank", children: _jsx("img", { className: 'imageCompanyes', src: `${BASE_URL}/empresas/tyrion.png` }) }), _jsx("img", { className: 'imageCompanyes', src: `${BASE_URL}/empresas/logo_liz.jpg` }), _jsx("a", { href: 'https://kasterz.com', target: "_blank", children: _jsx("img", { className: 'imageCompanyes', src: '/images/logosClientes/krastz.jpg' }) }), _jsx("img", { className: 'imageCompanyes', src: `${BASE_URL}/empresas/cra.png` }), _jsx("img", { className: 'imageCompanyes', src: `${BASE_URL}/empresas/uno.png` }), _jsx("a", { href: 'https://www.instagram.com/elephant_cun?igsh=MTN3aTZiOGJod3NkNA==', target: "_blank", children: _jsx("img", { className: 'imageCompanyes', src: `${BASE_URL}/empresas/elephant.png` }) })] })] }));
-};
-export default PartnerCompany;
